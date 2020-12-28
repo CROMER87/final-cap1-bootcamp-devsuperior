cfe0511e470a7b63de191d22f93b32591c4bfe8e
CROMER87 <mero.rjf@hotmail.com>
2020-12-28 19:08:26 -0300

    Projeto Criado

---
+++ final_cap1/src/main/java/com/romer/final_cap1/FinalCap1Application.java
@@ -0,0 +1,13 @@
+package com.romer.final_cap1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalCap1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalCap1Application.class, args);
+	}
+
+}
